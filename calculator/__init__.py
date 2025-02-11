d10d008ba1af92d9aec4a58b1c81f76489d4a736
yash-kamlesh-shah <yks@njit.edu>
2025-02-10 20:15:34 -0500

    part 3 code commit

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,5 +1,28 @@
-def add(a,b):
-    return a + b
+from calculator.calculation_history import Calculations
+from calculator.operations import add, subtract, multiply, divide
+from calculator.calculation import Calculation
+from decimal import Decimal
+from typing import Callable
 
-def subtract(a,b):
-    return a - b
+class Calculator:
+    @staticmethod
+    def _perform_operation(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]) -> Decimal:
+        calculation = Calculation.create(a, b, operation)
+        Calculations.add_calculation(calculation)
+        return calculation.perform()
+
+    @staticmethod
+    def add(a: Decimal, b: Decimal) -> Decimal:
+        return Calculator._perform_operation(a, b, add)
+
+    @staticmethod
+    def subtract(a: Decimal, b: Decimal) -> Decimal:
+        return Calculator._perform_operation(a, b, subtract)
+
+    @staticmethod
+    def multiply(a: Decimal, b: Decimal) -> Decimal:
+        return Calculator._perform_operation(a, b, multiply)
+
+    @staticmethod
+    def divide(a: Decimal, b: Decimal) -> Decimal:
+        return Calculator._perform_operation(a, b, divide)
